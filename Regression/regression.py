756be5c6e30b8ade8b8fcc955627f225d2e08b56
Alvaro Olivie <201905789@alu.comillas.edu>
2024-04-15 16:38:11 +0200

    lasso and ridge

--- Regression/regression.py
+++ Regression/regression.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, Lasso, ElasticNet, Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -34,51 +35,73 @@ def linear_regression(X, y):
     return mse, r2, hit_ratio
 
 
-def ridge_regression(X, y):
-    X_scaled = preprocessing.scale(X)
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-    
-    model = Ridge()
+def lasso_regression(X, y_penalized): 
+    X_penalized = preprocessing.scale(X.values)
+    n_alphas = 50 # declare the number of alphas for ridge
+    alphas = np.logspace(-4,-2,n_alphas) # here alpha is used for lambda in scikit-learn
+    lasso_res = {} # declaring the dict that will receive the model's result 
 
-    model.fit(X_train, y_train)
+    for alpha in alphas: # looping through the different alphas/lambdas values
+        lasso = Lasso(alpha=alpha) # model
+        lasso.fit(X_penalized,y_penalized) 
+        lasso_res[alpha] = lasso.coef_ # extract LASSO coefs
 
-    y_pred = model.predict(X_test)
-
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    hit_ratio = (y_test * y_pred > 0).mean()
-
-    coefs = pd.DataFrame(model.coef_)
-
-    print(coefs)
+    df_lasso_res = pd.DataFrame.from_dict(lasso_res).T # transpose the dataframe for plotting
+    df_lasso_res.columns = X.columns # adding the names of the factors
+    predictors = (df_lasso_res.abs().sum() > 0.05) # selecting the most relevant
+    df_lasso_res.loc[:,predictors].plot(xlabel='Lambda',ylabel='Beta',figsize=(12,8)); # Plot!
 
     plt.figure()
-    plt.title("Coefficient values vs Penalization for " + y.name + " returns")
-    plt.plot(coefs)
+    plt.title("Coefficient values vs Lasso Penalization for " + y_penalized.name + " returns")
+    df_lasso_res.loc[:,predictors].plot(xlabel='Lambda',ylabel='Beta',figsize=(13,8)) # Plot!
     plt.xlabel("Penalization")
-    plt.ylabel("Coefficient values")
+    plt.ylabel("Coefficient values")    
     plt.legend(X.columns)
-    plt.savefig("Regression/coefficient_values_" + y.name + ".png", format="png")
+    plt.savefig("Regression/lasso_values_" + y_penalized.name + ".png", format="png")
     plt.close()
 
-    return mse, r2, hit_ratio
+    
+
+def ridge_regression(X, y_penalized):
+    X_penalized = preprocessing.scale(X.values)
+    n_alphas = 50 # declare the number of alphas for ridge
+    alphas = np.logspace(-2, 5, n_alphas) # transforming into log for Aspect ratio 
+    ridge_res = {} # declaring the dict that will receive the model's result 
+
+    for alpha in alphas: # looping through the different alphas/lambdas values
+        ridge = Ridge(alpha=alpha) # model
+        ridge.fit(X_penalized,y_penalized) # fit the model
+        ridge_res[alpha] = ridge.coef_ # extract RIDGE coefs
+
+    df_ridge_res = pd.DataFrame.from_dict(ridge_res).T # transpose the dataframe for plotting
+    df_ridge_res.columns = X.columns # adding the names of the factors
+    predictors = (df_ridge_res.abs().sum() > 0.05) # selecting the most relevant
+
+    plt.figure()
+    plt.title("Coefficient values vs Ridge Penalization for " + y_penalized.name + " returns")
+    df_ridge_res.loc[:,predictors].plot(xlabel='Lambda',ylabel='Beta',figsize=(13,8)) # Plot!
+    plt.xlabel("Penalization")
+    plt.ylabel("Coefficient values")    
+    plt.legend(X.columns)
+    plt.savefig("Regression/ridge_values_" + y_penalized.name + ".png", format="png")
+    plt.close()
 
 
 if __name__ == '__main__':
     returns = ['R1M', 'R3M', 'R6M', 'R12M']
-    results = pd.DataFrame(columns=['Model', 'Y', 'MSE', 'R2', 'Hit Ratio'])
+    results = pd.DataFrame(columns=['Target', 'MSE', 'R2', 'Hit Ratio'])
 
     for i in returns:
         y = data[i]
         x = data.select_dtypes(include='number').drop(returns, axis=1)
         
         mse, r2, hit_ratio = linear_regression(x, y)
-        results = results._append({'Model': 'Linear Regression', 'Y': i, 'MSE': mse, 'R2': r2, 'Hit Ratio': hit_ratio}, ignore_index=True)
+        results = results._append({'Target': i, 'MSE': mse, 'R2': r2, 'Hit Ratio': hit_ratio}, ignore_index=True)
         
-        mse_ridge, r2_ridge, hit_ratios_ridge = ridge_regression(x, y)
-        results = results._append({'Model': 'Ridge Regression', 'Y': i, 'MSE': mse_ridge, 'R2': r2_ridge, 'Hit Ratio': hit_ratios_ridge}, ignore_index=True)
+        ridge_regression(x, y)
+        lasso_regression(x, y)
 
-    print(results)
+    results.to_csv('Regression/results.csv', index=False)
 
 
 
